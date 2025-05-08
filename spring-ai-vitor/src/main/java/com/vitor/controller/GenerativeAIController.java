6889af07f81e631611afffeb2c6794dac0e432c3
VitorCostaVianna <vcvianna@sga.pucmminas.br>
2025-05-08 15:23:55 -0300

    request mapping (ai)

--- spring-ai-vitor/src/main/java/com/vitor/controller/GenerativeAIController.java
+++ spring-ai-vitor/src/main/java/com/vitor/controller/GenerativeAIController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.ai.image.ImageResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import com.vitor.service.ImageService;
 import com.vitor.service.RecipeService;
 
 @RestController
+@RequestMapping("ai")
 public class GenerativeAIController {
     
     private final ChatService chatService;
